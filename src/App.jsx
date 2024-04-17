00a79847eaaf7b139d54c360b1402b46c4de066a
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-04-17 22:23:56 +0530

    add: header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Banner from "./components/Banner/Banner"
 import Header from "./components/Header/Header"
 
 function App() {
   return (
-    <div>
+    <div className="bg-blue-400">
       <Header />
     </div>
 
